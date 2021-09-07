333b4869561652240dd1c4f55c8e0e9a337d09ae
tlebryk <43556997+tlebryk@users.noreply.github.com>
2021-09-07 08:28:54 -0400

    cybernews/models/ not models/

--- rank.py
+++ rank.py
@@ -4,8 +4,11 @@ import json
 from datetime import date, datetime, timedelta
 import os
 
-analyze = pickle.load(open("models/analyze2.pickle", "rb"))
-gbm = pickle.load(open("models/gbm2.pickle", "rb"))
+HOMEDIR = os.path.expanduser("~")
+
+
+analyze = pickle.load(open("cybernews/models/analyze2.pickle", "rb"))
+gbm = pickle.load(open("cybernews/models/gbm2.pickle", "rb"))
 
 # takes df, generates rankings, and returns sorted df based on rankings
 def sort(df):
