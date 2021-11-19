8c1911f81a28e4bc4de0d055cd92031b16f2f7ea
tlebryk <43556997+tlebryk@users.noreply.github.com>
2021-11-18 20:49:14 -0500

    add

---
+++ app/rank.py
@@ -0,0 +1,23 @@
+import pickle
+from pandas import DataFrame
+import json
+from datetime import date, datetime, timedelta
+import os
+
+HOMEDIR = os.path.expanduser("~")
+
+
+analyze = pickle.load(open("app/models/analyze2.pickle", "rb"))
+gbm = pickle.load(open("app/models/gbm2.pickle", "rb"))
+
+# takes df, generates rankings, and returns sorted df based on rankings
+
+
+def sort(df):
+    trans = analyze.transform(df["body"]).astype('float32')
+    preds = gbm.predict(trans)
+    print(preds)
+    df['preds'] = preds
+    df.fillna(value="None", inplace=True)
+    # df = df.drop("Tags", axis=1)
+    return df.sort_values("preds")
